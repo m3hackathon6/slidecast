6349c0fcf571a4d1f1afc8a0c1bea417d6380145
Chris Birchall <c-birchall@m3.com>
2014-10-16 12:12:41 +0900

    Refactoring: remove logic from routes file
    Use EJS template to generate the injected HTML

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var presentations = require('../lib/presentations');
-var request = require('request');
-var q = require('q');
+var controller = require('../lib/controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,47 +9,17 @@ router.get('/', function(req, res) {
 
 /* View a presentation */
 router.get('/presentations/:id', function(req, res) {
-  presentations.findById(req.params.id)
-    .then(function(presentation) {
-      if (!presentation) {
-        res.status(404);
-      } else {
-        // TODO move all this into a service
-        request(presentation.url, function(error, response, body) {
-          // TODO make this slightly less hacky
-          var injectedStuff = '<a id="slidecast-data" data-pres-id="' + presentation.presId + '"></a><script src="/socket.io/socket.io.js"></script><script src="/javascripts/slidecast.slideshare.js"></script>';
-          var bodyWithInjection = body.replace("</body>", injectedStuff + "</body>");
-          res.send(bodyWithInjection);
-        });
-      }
-    });
+  controller.viewPresentation(res, req.params.id);
 });
 
+/* Show the password form for presenting a presentation */
 router.get('/presentations/:id/present', function(req, res) {
   res.render('password-form', { });
 });
 
 /* Present a presentation */
 router.post('/presentations/:id/present', function(req, res) {
-  presentations.findById(req.params.id)
-    .then(function(presentation) {
-      if (!presentation) {
-        res.status(404);
-      } else {
-        if (req.body.password != presentation.password) {
-          // Redirect back to password form
-          res.redirect(req.path);
-        } else {
-          // TODO move all this into a service
-          request(presentation.url, function(error, response, body) {
-            // TODO make this slightly less hacky
-            var injectedStuff = '<a id="slidecast-data" data-pres-id="' + presentation.presId + '" data-presenter="true" data-presenter-key="' + presentation.presenterKey + '"></a><script src="/socket.io/socket.io.js"></script><script src="/javascripts/slidecast.slideshare.js"></script>';
-            var bodyWithInjection = body.replace("</body>", injectedStuff + "</body>");
-            res.send(bodyWithInjection);
-          });
-        }
-      }
-    });
+  controller.presentPresentation(res, req.params.id, req.body.password, req.path);
 });
 
 module.exports = router;
