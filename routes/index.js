a890bc0794b7dfb2db2a49cbeb55e6ec2e304e8c
Chris Birchall <chris.birchall@gmail.com>
2014-10-20 22:07:16 +0900

    UA sniffing

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res) {
 
 /* View a presentation */
 router.get('/presentations/:id', function(req, res) {
-  controller.viewPresentation(res, req.params.id);
+  controller.viewPresentation(res, req.params.id, req.headers['user-agent']);
 });
 
 /* Show the password form for presenting a presentation */
@@ -19,7 +19,7 @@ router.get('/presentations/:id/present', function(req, res) {
 
 /* Present a presentation */
 router.post('/presentations/:id/present', function(req, res) {
-  controller.presentPresentation(res, req.params.id, req.body.password, req.path);
+  controller.presentPresentation(res, req.params.id, req.body.password, req.path, req.headers['User-Agent']);
 });
 
 module.exports = router;
